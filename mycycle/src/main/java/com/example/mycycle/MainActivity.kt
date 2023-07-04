62eff09954d9ec908c1c06f7d2bf007d4883b48d
NOOBC5 <zhemwlq00101@gmail.com>
2023-07-04 15:56:55 +0900

    activity

---
+++ mycycle/src/main/java/com/example/mycycle/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.mycycle
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.mycycle.databinding.ActivityMainBinding
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("MyActivity", "onCrate")
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val requestLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
+            it.data!!.getStringExtra("result")?.let {
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MyActivity", "onStart")
+    }    override fun onStop() {
+        super.onStop()
+        Log.d("MyActivity", "onStop")
+    }    override fun onResume() {
+        super.onResume()
+        Log.d("MyActivity", "onResume")
+    }    override fun onPause() {
+        super.onPause()
+        Log.d("MyActivity", "onPause")
+    }    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MyActivity", "onDestroy")
+    }    override fun onRestart() {
+        super.onRestart()
+        Log.d("MyActivity", "onRestart")
+    }
+}
+
+class TwoActivity {
+
+}
